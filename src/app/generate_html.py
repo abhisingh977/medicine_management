bd45d0be8a4d659ece8309656f6ee38018ea4c53
Abhishek Singh <abhis1997singh@gmail.com>
2023-12-29 03:33:57 +0530

    push

--- src/app/generate_html.py
+++ src/app/generate_html.py
@@ -6,24 +6,30 @@ def generate_html(dataframe: pd.DataFrame):
     # construct the complete HTML with jQuery DataTables
     html = f"""
     <html>
-    <header>
+    <head>
         <link href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
-    </header>
+        <link rel="stylesheet" type="text/css" href="/static/css/generate.css">
+    </head>
     <body>
     {table_html}
     <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+ 
     <script>
         $(document).ready( function () {{
             $('#table').DataTable({{
-                paging: false,    
-                scrollY: 1000,
+                paging: false,
+                scrollY: 10000,
+                
             }});
             $('div.dataTables_filter input').focus();
+
+            
         }});
     </script>
     </body>
     </html>
     """
-    # return the html
+
+
     return html
